8a9300a8ffeb07cce0c7cbb672b33dd2b804fd4e
Lia <juliareinaldi@outlook.com>
2020-09-29 22:31:38 -0300

    starting again

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import Footer from './components/footer';
+import Header from './components/header';
+import Myteams from './components/myteams';
+import FormTeam from './components/createTeam';
+import Top from './components/top';
+import Picked from './components/picked';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
+
+  function Home() {
+    return (
+      <div className="content-wrap">
+        <Container>
+          <Row>
+            <Col>
+              <Myteams />
+            </Col>
+            <Col>
+              <Top />
+              <Picked/>
+            </Col>
+          </Row>
+        </Container>
+        <Route path='/' exact />
+      </div>
+    );
+  }
+
+  function Create() {
+    return <FormTeam/>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/team/create">
+            <Create />
+          </Route>
+
+        </Switch>
+
+
+        <Footer />
+      </Router>
     </div>
+
   );
 }
 
